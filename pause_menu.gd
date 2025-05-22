0431c05da3b470b42e707675f9b0d9d2ccfdda13
Lord Ezen <mo23274@bristol.ac.uk>
2025-05-22 06:00:34 +0100

    Bug fix

--- pause_menu.gd
+++ pause_menu.gd
@@ -3,22 +3,26 @@ extends Control
 func _ready() :
 	$AnimationPlayer.play("RESET")
 	hide()
+	set_process_input(true)  # Make sure input is processed
+	
+func _input(event):
+	if event.is_action_pressed("pause"):
+		if visible :
+			resume()
+		else :
+			pause()
 
 func resume() :
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
+	await $AnimationPlayer.animation_finished
+	hide()
 	
 func pause() :
 	show()
 	get_tree().paused = true
 	$AnimationPlayer.play("blur")
-	
-func pausing():
-	if Input.is_action_just_pressed("pause") and !get_tree().paused:
-		pause()
-		
-	if Input.is_action_just_pressed("pause") and get_tree().paused:
-		resume()
+
 
 
 func _on_resume_pressed() -> void:
@@ -34,5 +38,3 @@ func _on_restart_pressed() -> void:
 func _on_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
 	
-func _process(delta):
-	pausing();
