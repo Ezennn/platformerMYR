42f333a9b9e7139afb8f4e91998bdd069ca26b50
KyeYongChoo <CHOO0207@e.ntu.edu.sg>
2025-05-28 08:46:22 +0800

    coin-console-fixes,-coin-console-fixes,-coin-console-fixes
    coin console fixes
    win screen display score
    display max score

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -2,16 +2,11 @@ extends Area2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var game_manager
 func _ready() -> void:
-	var managers = get_tree().get_nodes_in_group("GameManager")
-	if managers.size() > 0:
-		game_manager = managers[0]
-	else:
-		print("GameManager not found in group.")
+	GameManager.inc_max_score()
 		
 func _on_body_entered(_body: Node2D) -> void:
 	# waiting for the animation is too laggy
 	remove_child($AnimatedSprite2D)
-	game_manager.add_point()
+	GameManager.add_point()
 	animation_player.play("pickupAnimation")
--- scripts/crown.gd
+++ scripts/crown.gd
@@ -2,18 +2,10 @@ extends Area2D
 
 signal win(level : int)
 
-var game_manager
 @export var level : int = 1
-
-func _ready() -> void:
-	var managers = get_tree().get_nodes_in_group("GameManager")
-	if managers.size() > 0:
-		game_manager = managers[0]
-	else:
-		print("GameManager not found in group.")
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body is Player):
 		# currently the win signal isnt used by anyone. Most communication (Except killzones) are done using GameManager as a unique name 
 		emit_signal("win", level)
-		game_manager.win(level)
+		GameManager.win(level)
